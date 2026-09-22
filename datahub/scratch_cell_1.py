a1df0bdd7c3415f6fb1590b286fe8b276cd03510
sleonardich <sleonardich@gmail.com>
2026-09-22 00:53:15 -0700

    Create scratch_cell_1.py

---
+++ datahub/scratch_cell_1.py
@@ -0,0 +1,10 @@
+# SCRATCH CELL 1
+# (Optional) Feel free to import any extra libraries or packages here
+# However, you do not need to import any extra libraries to answer the question and receive full credit
+# Remember to comment out your code for step 3
+import polars as pl
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import textwrap
